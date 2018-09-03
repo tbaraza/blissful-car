66bfb9be335e3865ffc3c8f033c4899fb2c728a2
tbaraza <tonida.baraza@andela.com>
2018-09-03 20:44:23 +0300

    Add mock data

---
+++ backend/src/models/index.js
@@ -0,0 +1,53 @@
+module.exports = {
+  cars: [
+    {
+      passengers: 4,
+      insurance: 'basic',
+      bestFuel: 'true',
+      model: 'mercedes',
+      color: 'blue'
+    },
+    {
+      passengers: 2,
+      insurance: 'basic',
+      bestFuel: 'true',
+      model: 'mercedes',
+      color: 'blue'
+    },
+    {
+      passengers: 4,
+      insurance: 'basic',
+      bestFuel: 'true',
+      model: 'Porsche',
+      color: 'green'
+    },
+    {
+      passengers: 2,
+      insurance: 'basic',
+      bestFuel: 'false',
+      model: 'Land Rover',
+      color: 'green'
+    },
+    {
+      passengers: 4,
+      insurance: 'basic',
+      bestFuel: 'true',
+      model: 'Subaru',
+      color: 'green'
+    },
+    {
+      passengers: 4,
+      insurance: 'good',
+      bestFuel: 'true',
+      model: 'Volkswagen',
+      color: 'green'
+    },
+    {
+      passengers: 4,
+      insurance: 'premium',
+      bestFuel: 'true',
+      model: '',
+      color: 'green'
+    }
+  ]
+};
