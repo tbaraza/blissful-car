9ebc3dc7357465ee4bf27e5ac846e7ac7d3f4b23
tbaraza <tonida.baraza@andela.com>
2018-09-04 13:06:00 +0300

    Rename search reducer and actions

---
+++ frontend/src/pages/home/actions/searchActions.js
@@ -0,0 +1,39 @@
+import searchActionTypes from './actionTypes';
+import apiClient from '../../../api/apiClient';
+
+// selectors
+export const getCars = state => state.search;
+
+export const fetchSearchRequest = () => ({
+  type: searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST,
+});
+
+export const fetchSearchRequestSuccess = payload => ({
+  type: searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST_SUCCESS,
+  cars: payload.data,
+});
+
+export const fetchSearchRequestFail = error => ({
+  type: searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST_FAIL,
+  error,
+});
+
+export const fetchSearchResults = values => async (dispatch) => {
+  const { passengers, insurance, bestFuel } = values;
+
+  dispatch(fetchSearchRequest());
+
+  try {
+    const response = await apiClient(
+      'get',
+      `/search?passengers=${passengers}&insurance=${insurance}&bestFuel=${bestFuel}`,
+    );
+    dispatch(fetchSearchRequestSuccess(response));
+  } catch (error) {
+    if (error.response) {
+      dispatch(fetchSearchRequestFail(error.response));
+    } else {
+      dispatch(fetchSearchRequestFail(error));
+    }
+  }
+};
---
+++ frontend/src/pages/home/reducer/searchReducer.js
@@ -0,0 +1,36 @@
+import searchActionTypes from '../actions/actionTypes';
+
+const DEFAULT_STATE = {
+  cars: [],
+  errorObject: {},
+};
+
+const fetchSearch = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+  case searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST:
+    return {
+      ...state,
+      cars: [],
+      errorObject: {},
+    };
+
+  case searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST_SUCCESS:
+    return {
+      ...state,
+      cars: action.cars,
+      errorObject: {},
+    };
+
+  case searchActionTypes.FETCH_SEARCH_RESULTS_REQUEST_FAIL:
+    return {
+      ...state,
+      cars: [],
+      errorObject: action.error,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default fetchSearch;
