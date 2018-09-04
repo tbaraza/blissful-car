8c0c3509524fdb4d9198a7c1acd0be0be42e5a4f
tbaraza <tonida.baraza@andela.com>
2018-09-04 23:38:44 +0300

    Create socket.io events

---
+++ backend/src/services/analytics/socket.js
@@ -0,0 +1,40 @@
+const computeStats = require('./analytics');
+
+module.exports = (io) => {
+  const visitorsData = {};
+
+  io.on('connection', (socket) => {
+    // a user has visited our page - add them to the visitorsData object
+    console.log('a user connected');
+
+    if (
+      socket.handshake.headers.host === 'http://127.0.0.1:9000'
+      && socket.handshake.headers.referer.indexOf('http://127.0.0.1:9000' + '/dashboard') > -1
+    ) {
+      // if someone visits '/dashboard' send them the computed visitor data
+      io.emit('updated-stats', computeStats(visitorsData));
+    }
+    // a user has visited our page - add them to the visitorsData object
+
+    socket.on('visitor-data', (data) => {
+      visitorsData[socket.id] = data;
+      console.log('analytics---7', visitorsData);
+      socket.emit('visitor-data', data);
+
+      // compute and send visitor data to the dashboard when a new user visits our page
+      io.emit('updated-stats', computeStats(visitorsData));
+    });
+
+    socket.on('disconnect', () => {
+      // a user has left our page - remove them from the visitorsData object
+      delete visitorsData[socket.id];
+
+      // compute and send visitor data to the dashboard when a user leaves our page
+      io.emit('updated-stats', computeStats(visitorsData));
+    });
+  });
+
+  console.log('analytics', visitorsData);
+
+  return io;
+};
