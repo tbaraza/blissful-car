7288a55e6f4864c08474bb4853e19fc1e980cfc2
tbaraza <tonida.baraza@andela.com>
2018-09-05 19:20:27 +0300

    Add dashboard page

--- frontend/src/pages/dashboard/DashboardPage.js
+++ frontend/src/pages/dashboard/DashboardPage.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
-import { Card, Icon } from 'antd';
+import { Card, Icon, List } from 'antd';
+import socketIOClient from 'socket.io-client';
 import './DashboardPage.css';
 
 const { Meta } = Card;
 
 class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      endpoint: 'http://127.0.0.1:9000',
+      pages: {},
+      referrers: 0,
+      activeUsers: 0,
+      searches: 0,
+      visits: 0,
+    };
+  }
+
+  // componentDidMount() {
+  //   console.log('haiaaa');
+  //   const socket = socketIOClient(this.state.endpoint);
+  //   socket.on('updated-stats', (data) => {
+  //     console.log('client', data);
+  //     this.setState({
+  //       activeUsers: data.activeUsers,
+  //       referrers: Object.keys(data.referrers).length,
+  //       pages: Object.keys(data.pages).length,
+  //     });
+  //   });
+  // }
+
+  getStats = (data) => {
+    const {
+      pages, referrers, activeUsers, searches,
+    } = data;
+
+    console.log('data===', pages);
+    this.setState({
+      activeUsers,
+      referrers: Object.keys(referrers).length,
+      pages,
+      visits: Object.keys(pages).length,
+      searches,
+    });
+  };
+
   render() {
+    const {
+      pages, referrers, activeUsers, endpoint, searches, visits,
+    } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on('updated-stats', (data) => {
+      console.log('client', data);
+      this.getStats(data);
+    });
+
+    const data = [
+      {
+        page: '/',
+        stats: pages['/'] ? pages['/'] : 0,
+      },
+      {
+        page: '/deals',
+        stats: pages['/deals'] ? pages['/deals'] : 0,
+      },
+      {
+        page: '/dashboard',
+        stats: pages['/dashboard'] ? pages['/dashboard'] : 0,
+      },
+    ];
+
     return (
       <div className="dashboard-container">
-        <h2>Dashboard</h2>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <Card
             hoverable
@@ -19,7 +84,7 @@ class DashboardPage extends Component {
               </div>
             )}
           >
-            <Meta title="Site Visits" description="34" />
+            <Meta title="Site Visits" description={`${visits}`} />
           </Card>
 
           <Card
@@ -31,7 +96,7 @@ class DashboardPage extends Component {
               </div>
             )}
           >
-            <Meta title="Active Users" description="34" />
+            <Meta title="Active Users" description={`${activeUsers}`} />
           </Card>
 
           <Card
@@ -43,9 +108,29 @@ class DashboardPage extends Component {
               </div>
             )}
           >
-            <Meta title="Searches" description="34" />
+            <Meta title="Searches" description={`${searches}`} />
           </Card>
         </div>
+
+        <h2 style={{ color: 'gray' }}>Page stats</h2>
+
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <Card title={item.page}>{`${item.stats}`}</Card>
+            </List.Item>
+          )}
+        />
       </div>
     );
   }
