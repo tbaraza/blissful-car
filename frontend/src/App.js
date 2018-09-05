a54d0bc4aa6cfb0aa556198996cbc220842858a3
tbaraza <tonida.baraza@andela.com>
2018-09-05 19:20:48 +0300

    Add nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,21 @@ import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import HomePageContainer from './pages/home/HomePageContainer';
 import DealsPageContainer from './pages/deals/DealsPageContainer';
 import DashboardPage from './pages/dashboard/DashboardPage';
+import './App.css';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={HomePageContainer} />
-      <Route exact path="/deals" component={DealsPageContainer} />
-      <Route exact path="/dashboard" component={DashboardPage} />
-    </Switch>
-  </BrowserRouter>
+  <div>
+    <div className="container">
+      <p className="header">Blissful Car</p>
+    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePageContainer} />
+        <Route exact path="/deals" component={DealsPageContainer} />
+        <Route exact path="/dashboard" component={DashboardPage} />
+      </Switch>
+    </BrowserRouter>
+  </div>
 );
 
 export default App;
