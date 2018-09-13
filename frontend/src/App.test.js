0fa8cc86cfcc6ba6ea988536b168d4c7fcc2a8ac
tbaraza <tonida.baraza@andela.com>
2018-09-13 19:04:37 +0300

    Fix frontend test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,26 @@
+/* eslint-disable no-undef */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import configureMockStore from 'redux-mock-store';
 import App from './App';
+
+// React 16 Enzyme adapter
+
+Enzyme.configure({ adapter: new Adapter() });
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
